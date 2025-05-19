5780c550249fec1368e3a1c410ec8c2f9e63d617
eunzinn <dhr2176@gmail.com>
2025-05-19 21:13:05 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
         int strike = 0; //입력한 값의 strike를 저장받을 변수
         int ball = 0; //입력한 값의 ball을 입력받을 변수
 
+        System.out.println("추가됨");
+        System.out.println("추가됨");
+        System.out.println("추가됨");
+        System.out.println("추가됨");
+
+
         Scanner s = new Scanner(System.in);
         while(true) {
             //과정2 : inputArr.length만큼 반복해 유저가 입력한 숫자를 순서대로 inputArr에 넣는다.
@@ -14,19 +20,10 @@ public class Main {
                 System.out.println("1~9사이의 정수를 입력하세요 : "); //console에 안내문 출력
                 inputArr[i] = s.nextInt(); // scanner메서드를 이용해 inputArr에 유저가 입력한 숫자 넣기
                 for(int j = 0; j < i; j++) {
-                    if(inputArr[j] == inputArr[i]) {
-                        //유저가 입력한 i인덱스에 들어갈 숫자가 중복된 숫자인지 확인
-                        System.out.println("중복된 값을 입력했습니다.");
-                        i--;
-                        break;
-                    }
+
                 }
             }
 
-            System.out.println("추가됨");
-            System.out.println("추가됨");
-            System.out.println("추가됨");
-            System.out.println("추가됨");
 
             System.out.println(strike + "스트라이크" + ball + "볼");
             if(strike == 3) {
